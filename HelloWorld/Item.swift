4fb0632d37e962730e9b589b7c24a3b6fe36ad78
Mangee <bigshots001@Outlook.com>
2015-01-25 21:29:02 +0800

    Add item class

---
+++ HelloWorld/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  HelloWorld
+//
+//  Created by Fanta on 15-1-25.
+//  Copyright (c) 2015年 Mangju. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var name: String!
+    
+}
